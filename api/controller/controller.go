2c28ed4df25ae641b67a7c0d830b73186fe2903c
Andrii <zimnax@gmail.com>
2024-07-26 20:36:45 -0400

    added return object from submit quiz API

--- api/controller/controller.go
+++ api/controller/controller.go
@@ -76,7 +76,18 @@ func (gc *GeniusController) SubmitQuiz(ctx *fiber.Ctx) error {
 		a1SkinType, a2SkinSensitivity, a3Acne, a5Preference,
 		a6Allergy, a7Concerns, a4Age, a8Benefits)
 
-	fmt.Println(fmt.Sprintf("top 3: %#v", top3))
+	fmt.Println(fmt.Sprintf("top 3: %#v", len(top3)))
+
+	for _, topP := range top3 {
+		topP.Ingredients = nil
+	}
+
+	err := ctx.JSON(top3)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("failed to marshall top3 req, err: %+v", err))
+	}
+
+	return err
 
 	//iBySkinTypeList, err1 := gc.geniusData.IngredientBySkinType(ctx.UserContext(), qa.SkinType)
 	//fmt.Println(fmt.Sprintf("IngredientBySkinType: [%d], error: %v", len(iBySkinTypeList), err1))
@@ -102,29 +113,29 @@ func (gc *GeniusController) SubmitQuiz(ctx *fiber.Ctx) error {
 	//iByBenefit, err8 := gc.geniusData.IngredientByProductBenefit(ctx.UserContext(), qa.ProductBenefit)
 	//fmt.Println(fmt.Sprintf("iByBenefit: [%d], error: %v", len(iByBenefit), err8))
 
-	allSkinTypes, err1 := gc.geniusData.GetAllSkintypes(ctx.UserContext())
-	fmt.Println(fmt.Sprintf("allSkinTypes: [%d], error: %v", allSkinTypes, err1))
-
-	allSkinSensetivities, err2 := gc.geniusData.GetAllSkinsensetivity(ctx.UserContext())
-	fmt.Println(fmt.Sprintf("allSkinSensetivities: [%d], error: %v", allSkinSensetivities, err2))
-
-	allAcneBreakouts, err3 := gc.geniusData.GetAllSkinsensetivity(ctx.UserContext())
-	fmt.Println(fmt.Sprintf("allAcneBreakouts: [%d], error: %v", allAcneBreakouts, err3))
-
-	allPreferences, err4 := gc.geniusData.GetAllPreferences(ctx.UserContext())
-	fmt.Println(fmt.Sprintf("allPreferences: [%d], error: %v", allPreferences, err4))
-
-	allAllergies, err5 := gc.geniusData.GetAllAllergies(ctx.UserContext())
-	fmt.Println(fmt.Sprintf("allAlergies: [%d], error: %v", allAllergies, err5))
-
-	allSkinconcerns, err6 := gc.geniusData.GetAllSkinconcerns(ctx.UserContext())
-	fmt.Println(fmt.Sprintf("allSkinconcerns: [%d], error: %v", allSkinconcerns, err6))
-
-	allAges, err7 := gc.geniusData.GetAllAge(ctx.UserContext())
-	fmt.Println(fmt.Sprintf("allAges: [%d], error: %v", allAges, err7))
-
-	allBenefits, err8 := gc.geniusData.GetAllBenefits(ctx.UserContext())
-	fmt.Println(fmt.Sprintf("allBenefits: [%d], error: %v", allBenefits, err8))
-
-	return nil
+	//allSkinTypes, err1 := gc.geniusData.GetAllSkintypes(ctx.UserContext())
+	//fmt.Println(fmt.Sprintf("allSkinTypes: [%d], error: %v", allSkinTypes, err1))
+	//
+	//allSkinSensetivities, err2 := gc.geniusData.GetAllSkinsensetivity(ctx.UserContext())
+	//fmt.Println(fmt.Sprintf("allSkinSensetivities: [%d], error: %v", allSkinSensetivities, err2))
+	//
+	//allAcneBreakouts, err3 := gc.geniusData.GetAllSkinsensetivity(ctx.UserContext())
+	//fmt.Println(fmt.Sprintf("allAcneBreakouts: [%d], error: %v", allAcneBreakouts, err3))
+	//
+	//allPreferences, err4 := gc.geniusData.GetAllPreferences(ctx.UserContext())
+	//fmt.Println(fmt.Sprintf("allPreferences: [%d], error: %v", allPreferences, err4))
+	//
+	//allAllergies, err5 := gc.geniusData.GetAllAllergies(ctx.UserContext())
+	//fmt.Println(fmt.Sprintf("allAlergies: [%d], error: %v", allAllergies, err5))
+	//
+	//allSkinconcerns, err6 := gc.geniusData.GetAllSkinconcerns(ctx.UserContext())
+	//fmt.Println(fmt.Sprintf("allSkinconcerns: [%d], error: %v", allSkinconcerns, err6))
+	//
+	//allAges, err7 := gc.geniusData.GetAllAge(ctx.UserContext())
+	//fmt.Println(fmt.Sprintf("allAges: [%d], error: %v", allAges, err7))
+	//
+	//allBenefits, err8 := gc.geniusData.GetAllBenefits(ctx.UserContext())
+	//fmt.Println(fmt.Sprintf("allBenefits: [%d], error: %v", allBenefits, err8))
+	//
+	//return nil
 }
