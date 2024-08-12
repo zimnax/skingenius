03cb9a8c924891f769c7f535ad35484f47c15e66
Andrii <zimnax@gmail.com>
2024-08-12 19:34:14 -0400

    updated bin

--- api/routes/skingenius.go
+++ api/routes/skingenius.go
@@ -12,6 +12,6 @@ func GeniusRoutes(a *fiber.App, gc *controller.GeniusController) {
 	route.Post("/user/:id/recommendation", gc.SaveRecommendation)
 	route.Get("/user/:id/recommendation", gc.GetRecommendation)
 	route.Post("/user/:id/quiz", gc.SaveQuiz)
-	route.Get("/user/:id/quiz", gc.GetRecommendation)
+	route.Get("/user/:id/quiz", gc.GetQuiz)
 
 }
